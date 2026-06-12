8c085db7b3fa317352bee42b9c2ed019c56d0858
Skyron <courtier.leo@gmail.com>
2026-06-12 14:49:52 +0200

    chore(server): make constants inernals

--- server/server/Utils/Constants.cs
+++ server/server/Utils/Constants.cs
@@ -1,8 +1,8 @@
 ﻿namespace server.Utils;
 
-public sealed class Constants
+internal static class Constants
 {
-    public static readonly string ServerBaseUrl = "http://localhost:5268";
+    internal static readonly string ServerBaseUrl = "http://localhost:5268";
     
-    public static readonly DateTime Birthday = new(2002, 11, 9);
+    internal static readonly DateTime Birthday = new(2002, 11, 9);
 }
