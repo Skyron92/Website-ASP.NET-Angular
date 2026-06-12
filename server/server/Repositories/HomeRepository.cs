a6eaa7f7053db281b5a62a93b3d1883c53be476c
Skyron <courtier.leo@gmail.com>
2026-06-12 16:28:23 +0200

    style: change text content

--- server/server/Repositories/HomeRepository.cs
+++ server/server/Repositories/HomeRepository.cs
@@ -14,8 +14,8 @@ public class HomeRepository
         if(today.DayOfYear < birthday.DayOfYear) age--;
 
         Home home = new Home {
-            Title = "Développeur web et 3D temps réel",
-            Presentation = $"Bienvenue sur mon portfolio. Je m'appelle Léo et j'ai {age} ans. Passionné par l'innovation, j'aspire à deux choses : poser les briques de l'avenir et simplifier celles qui sont déjà posées.\n\nCurieux de nature, mon parcours m’a conduit du dessin à la 3D, avant d'arriver à la programmation. D'abord dans le développement de jeux vidéo puis je me suis étendu au développement web.\nAujourd'hui, j'élargi toujours plus mon champs de compétences vers les voies qui m'attirent !",
+            Title = "Développeur fullstack",
+            Presentation = $"Je suis Léo et j'ai {age} ans.",
             ImageUrl = $"{Constants.ServerBaseUrl}/images/profile.png"
         };
         return Task.FromResult(home);
